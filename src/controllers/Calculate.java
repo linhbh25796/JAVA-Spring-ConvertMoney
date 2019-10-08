3b04db2f377a36ef9ce7c729cedbd09478678bae
linhbh25796 <54263035+linhbh25796@users.noreply.github.com>
2019-10-08 20:29:56 +0700

    commit

---
+++ src/controllers/Calculate.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Calculate {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/convert")
+    public String convertCurrency(@RequestParam String usd, Model model){
+        double vnd = Double.parseDouble(usd) * 22000;
+        model.addAttribute("vnd", vnd);
+
+        return "index";
+    }
+}
